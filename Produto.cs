7d909cf6e4ca05db186063e5c19e1aca9ad8790f
paulooosf <phpessoal2021@gmail.com>
2024-11-22 18:56:16 -0300

    feat: estruturação inicial da classe Produto

---
+++ Produto.cs
@@ -0,0 +1,33 @@
+﻿class Produto
+{
+    string Nome { get; set; }
+    double Preco { get; set; }
+    string Descricao { get; set; }
+    int Quantidade { get; set; }
+
+    public Produto (string nome, double preco, string descricao, int quantidade)
+    {
+        this.Nome = nome;
+        this.Preco = preco;
+        this.Descricao = descricao;
+        this.Quantidade = quantidade;
+    }
+
+    public void AdicionarQuantidade(int numero)
+    {
+        if (numero <= 0)
+        {
+            throw new ArgumentNullException("Defina uma quantidade correta!");
+        } 
+        this.Quantidade += numero;
+    }
+
+    public void RemoverQuantidade(int numero)
+    {
+        if (numero <= 0)
+        {
+            throw new ArgumentNullException("Defina uma quantidade correta!");
+        }
+        this.Quantidade -= numero;
+    }
+}
