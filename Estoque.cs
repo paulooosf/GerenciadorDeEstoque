d3853ed46f78354ab4c0426d69dec00e39e3afed
paulooosf <phpessoal2021@gmail.com>
2024-11-23 20:58:13 -0300

    feat: Adicionando classe Estoque

---
+++ Estoque.cs
@@ -0,0 +1,24 @@
+﻿class Estoque
+{
+    public List<Produto>? ListaProdutos { get; set; }
+
+    public Estoque()
+    {
+        ListaProdutos = new List<Produto>();
+    }
+
+    public void ListarProdutos()
+    {
+        if (ListaProdutos.Count == 0)
+        {
+            Console.WriteLine("\nNenhum produto cadastrado até o momento!");
+        }
+        else
+        {
+            foreach (var produto in ListaProdutos)
+            {
+                produto.ExibirInformacoes();
+            }
+        }
+    }
+}
