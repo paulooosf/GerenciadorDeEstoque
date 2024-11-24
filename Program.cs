db6fe58d240b9d6117007ae82f0a44a7765d171f
paulooosf <phpessoal2021@gmail.com>
2024-11-23 23:27:07 -0300

    feat: Estruturando método Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Estoque estoque = new();
+
+        // Inserindo produtos para fins de teste
+        estoque.CadastrarProduto(new("CD/DVD", 12.99, "CD/DVD que pode ser reescrito", 10));
+        estoque.CadastrarProduto(new("Notebook", 1780.90, "Notebook Intel Core i7", 5));
+
+        Menu.MenuPrincipal(estoque);
+    }
+}
